35d2d2b4ed3f28f5ff787ed79e6a0088a22d8c97
Mery-R <smilosz11@gmail.com>
2025-06-09 22:23:23 +0200

    Start

---
+++ Views/Admin_Menu.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Car_Rental.Views
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Admin_Menu.xaml
+    /// </summary>
+    public partial class Admin_Menu : Window
+    {
+        public Admin_Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
