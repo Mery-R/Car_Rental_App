2e442e707689b10f344a18322f0e4c86794b8ecc
agataskalska <agataskalska@student.agh.edu.pl>
2025-06-21 17:11:41 +0200

    Add files via upload

---
+++ Views/Show_Cars_User_Control.xaml.cs
@@ -0,0 +1,167 @@
+﻿using Car_Rental.Models;
+using Car_Rental.Repositories;
+using Car_Rental.ViewModels;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+
+namespace Car_Rental.Views
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Show_Cars_User_Control.xaml
+    /// </summary>
+    public partial class Show_Cars_User_Control : UserControl
+    {
+        private readonly CarRepository _carRepository = new CarRepository();
+        public ShowCarsUserViewModel ViewModel { get; set; }
+
+        public Popup FiltersPopupPublic => FiltersPopup;
+
+        public Show_Cars_User_Control()
+        {
+            InitializeComponent();
+
+            var cars = _carRepository.GetAllCars();
+            ViewModel = new ShowCarsUserViewModel(cars);
+            this.DataContext = ViewModel;
+        }
+
+        private void SearchTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            // Implementacja jeśli potrzebna
+        }
+
+        private void SearchTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            // Implementacja jeśli potrzebna
+        }
+
+        private void RentCarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (CarDataGrid.SelectedItem is CarModel selectedCar)
+            {
+                var rentWindow = new Rent_Car_Window(selectedCar.CarId);
+                bool? result = rentWindow.ShowDialog();
+
+                if (result == true)
+                {
+                    RefreshCars();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Choose a car to rent.", "Info", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void ReturnCarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (CarDataGrid.SelectedItem is CarModel selectedCar)
+            {
+                var status = (CarStatus)selectedCar.StatusCar;
+                if (status != CarStatus.Rented)
+                {
+                    MessageBox.Show("This car is not currently rented.", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+                    return;
+                }
+
+                var reservationRepo = new ReservationRepository();
+                var latestReservation = reservationRepo.GetActiveReservationByCarId(selectedCar.CarId);
+
+                if (latestReservation == null)
+                {
+                    MessageBox.Show("No active reservation found for this car.", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+                    return;
+                }
+
+                var customerRepo = new CustomerRepository();
+                var customer = customerRepo.GetCustomerById(latestReservation.CustomerId);
+
+                if (customer == null)
+                {
+                    MessageBox.Show("Customer not found.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+                var returnWindow = new Return_Car_Window(selectedCar, customer, latestReservation);
+                if (returnWindow.ShowDialog() == true)
+                {
+                    RefreshCars();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Select a car to return.", "Info", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void FiltersButton_Click(object sender, RoutedEventArgs e)
+        {
+            FiltersPopup.IsOpen = !FiltersPopup.IsOpen;
+        }
+
+        private void FilterCheckBox_Changed(object sender, RoutedEventArgs e)
+        {
+            if (sender is CheckBox cb && Enum.TryParse<CarStatus>(cb.Content.ToString(), out var status))
+            {
+                switch (status)
+                {
+                    case CarStatus.Available:
+                        ViewModel.IsAvailableChecked = cb.IsChecked == true;
+                        break;
+                    case CarStatus.Reserved:
+                        ViewModel.IsReservedChecked = cb.IsChecked == true;
+                        break;
+                    case CarStatus.Rented:
+                        ViewModel.IsRentedChecked = cb.IsChecked == true;
+                        break;
+                   
+                }
+            }
+        }
+
+        public void RefreshCars()
+        {
+            var cars = _carRepository.GetAllCars();
+
+            ViewModel.AllCars.Clear();
+            foreach (var car in cars)
+            {
+                ViewModel.AllCars.Add(car);
+            }
+
+            ViewModel.ApplyFilter();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (FiltersPopup.IsOpen && !IsClickInsidePopup(e))
+            {
+                FiltersPopup.IsOpen = false;
+            }
+        }
+
+        private bool IsClickInsidePopup(MouseButtonEventArgs e)
+        {
+            if (FiltersPopup.Child is UIElement popupContent)
+            {
+                Point clickPos = e.GetPosition(popupContent);
+                Rect bounds = new Rect(0, 0, popupContent.RenderSize.Width, popupContent.RenderSize.Height);
+                return bounds.Contains(clickPos);
+            }
+            return false;
+        }
+
+        private void CarDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // opcjonalna logika
+        }
+
+        private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // opcjonalna logika
+        }
+    }
+}
--- Views/User_Menu.xaml.cs
+++ Views/User_Menu.xaml.cs
@@ -1,19 +1,13 @@
 ﻿// Plik: User_Menu.xaml.cs
 // Przeznaczenie: Okno menu użytkownika. Umożliwia dostęp do przeglądania pojazdów, historii wypożyczeń oraz wylogowania.
 
+using Car_Rental.Models;
+using Car_Rental.ViewModels;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.Windows.Interop;
 
 namespace Car_Rental.Views
 {
@@ -22,36 +16,146 @@ namespace Car_Rental.Views
     /// </summary>
     public partial class User_Menu : Window
     {
-        /// <summary>
-        /// Konstruktor inicjalizujący komponenty okna menu użytkownika.
-        /// </summary>
+        public UserAccountModel CurrentUserAccount { get; set; }
+        
         public User_Menu()
         {
             InitializeComponent();
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+
+            CurrentUserAccount = new UserAccountModel
+            {
+                Username = "user",
+                DisplayName = "Użytkownik"
+            };
+
+            this.DataContext = new MainViewModel();
+            this.SourceInitialized += User_Menu_SourceInitialized;
         }
 
-        /// <summary>
-        /// Obsługuje wyświetlanie dostępnych pojazdów.
-        /// </summary>
-        private void ShowVehicles_Click(object sender, RoutedEventArgs e)
+        // Wylogowanie
+        private void LogoutButton_Click(object sender, RoutedEventArgs e)
         {
-           //
+            var loginScreen = new MainWindow();
+            loginScreen.Show();
+            this.Close();
         }
 
-        /// <summary>
-        /// Obsługuje wyświetlanie historii wypożyczeń.
-        /// </summary>
-        private void History_Click(object sender, RoutedEventArgs e)
+        // Obsługa przycisku minimalizacji
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
         {
-            //
+            this.WindowState = WindowState.Minimized;
         }
 
-        /// <summary>
-        /// Obsługuje wylogowanie użytkownika.
-        /// </summary>
-        private void LogOut_Click(object sender, RoutedEventArgs e)
+        // Obsługa przycisku maksymalizacji
+        private void btnMaximize_Click(object sender, RoutedEventArgs e)
         {
-            //
+            this.WindowState = this.WindowState == WindowState.Normal
+                ? WindowState.Maximized
+                : WindowState.Normal;
+        }
+
+        // Obsługa przycisku zamknięcia
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        // Przeciąganie okna
+        private void pnlControlBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            WindowInteropHelper helper = new WindowInteropHelper(this);
+            SendMessage(helper.Handle, 161, 2, 0);
+        }
+
+        private void pnlControlBar_MouseEnter(object sender, MouseEventArgs e)
+        {
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        private void User_Menu_SourceInitialized(object sender, EventArgs e)
+        {
+            IntPtr handle = (new WindowInteropHelper(this)).Handle;
+            HwndSource hwndSource = HwndSource.FromHwnd(handle);
+            if (hwndSource != null)
+                hwndSource.AddHook(WindowProc);
+        }
+
+        // Resize logika
+        private const int HTLEFT = 10, HTRIGHT = 11, HTTOP = 12,
+                          HTTOPLEFT = 13, HTTOPRIGHT = 14, HTBOTTOM = 15,
+                          HTBOTTOMLEFT = 16, HTBOTTOMRIGHT = 17;
+
+        private const int WM_NCHITTEST = 0x0084;
+        private const int RESIZE_HANDLE_SIZE = 8;
+
+        private IntPtr WindowProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            if (msg == WM_NCHITTEST)
+            {
+                handled = true;
+                Point mousePos = GetMousePosition(lParam);
+
+                GetWindowRect(hwnd, out RECT windowRect);
+                Rect rect = new Rect(windowRect.Left, windowRect.Top,
+                                     windowRect.Right - windowRect.Left,
+                                     windowRect.Bottom - windowRect.Top);
+
+                if (mousePos.Y >= rect.Top && mousePos.Y <= rect.Top + RESIZE_HANDLE_SIZE)
+                {
+                    if (mousePos.X <= rect.Left + RESIZE_HANDLE_SIZE)
+                        return (IntPtr)HTTOPLEFT;
+                    if (mousePos.X >= rect.Right - RESIZE_HANDLE_SIZE)
+                        return (IntPtr)HTTOPRIGHT;
+                    return (IntPtr)HTTOP;
+                }
+
+                if (mousePos.Y >= rect.Bottom - RESIZE_HANDLE_SIZE)
+                {
+                    if (mousePos.X <= rect.Left + RESIZE_HANDLE_SIZE)
+                        return (IntPtr)HTBOTTOMLEFT;
+                    if (mousePos.X >= rect.Right - RESIZE_HANDLE_SIZE)
+                        return (IntPtr)HTBOTTOMRIGHT;
+                    return (IntPtr)HTBOTTOM;
+                }
+
+                if (mousePos.X <= rect.Left + RESIZE_HANDLE_SIZE)
+                    return (IntPtr)HTLEFT;
+                if (mousePos.X >= rect.Right - RESIZE_HANDLE_SIZE)
+                    return (IntPtr)HTRIGHT;
+
+                return (IntPtr)1; // HTCLIENT
+            }
+
+            return IntPtr.Zero;
+        }
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool GetWindowRect(IntPtr hwnd, out RECT lpRect);
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct RECT
+        {
+            public int Left, Top, Right, Bottom;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int X, Y;
+        }
+
+        [DllImport("user32.dll")]
+        public static extern bool GetCursorPos(out POINT lpPoint);
+
+        private Point GetMousePosition(IntPtr lParam)
+        {
+            GetCursorPos(out POINT pt);
+            return new Point(pt.X, pt.Y);
         }
     }
 }
